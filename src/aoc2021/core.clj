2bd5fe0c3c8881acd691034d3e42938e711a38d5
John Flinchbaugh <john@hjsoft.com>
2021-11-21 18:33:22 -0500

    docs

--- src/aoc2021/core.clj
+++ src/aoc2021/core.clj
@@ -2,24 +2,33 @@
   (:require [clojure.string :as str]))
 
 (defn parse-int
+  "parse an string into an integer"
   ([i]
    (Integer/parseInt i))
   ([i radix]
    (Integer/parseInt i radix)))
 
 (defn parse-long
+  "parse a string into a long"
   ([i]
    (Long/parseLong i))
   ([i radix]
    (Long/parseLong i radix)))
 
-(defn avg [coll]
+(defn avg
+  "average of values in a list"
+  [coll]
   (when (not (empty? coll))
     (/ (reduce + coll) (count coll))))
 
-(defn abs [n] (Math/abs n))
+(defn abs
+  "absolute value"
+  [n]
+  (Math/abs n))
 
-(defn file->lines [file-name]
+(defn file->lines
+  "read a local named file and parse it into trimmed lines"
+  [file-name]
   (->> file-name
     slurp
     str/trim
