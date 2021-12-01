7f2336de651b211230069cecbba9f2e06d337bf5
John Flinchbaugh <john@hjsoft.com>
2021-12-01 06:31:55 -0500

    extract named fns

--- src/aoc2021/day_01.clj
+++ src/aoc2021/day_01.clj
@@ -1,28 +1,39 @@
 (ns aoc2021.day-01
   (:require [aoc2021.core :refer :all]))
 
+(defn changes [c]
+  (->>
+    c
+    (partition 2 1)
+    (map (comp #(reduce - %) reverse)))
+  )
+
 (defn part-1 []
   (->>
     "src/aoc2021/day_01.txt"
     file->lines
     (map parse-int)
-    (partition 2 1)
-    (map #(reduce - %))
-    (filter neg?)
+    changes
+    (filter pos?)
     count
     )
   )
+
+(defn sliding-window-sum [size c]
+  (->>
+    c
+    (partition size 1)
+    (map #(reduce + %))
+    ))
 
 (defn part-2 []
   (->>
     "src/aoc2021/day_01.txt"
     file->lines
     (map parse-int)
-    (partition 3 1)
-    (map #(reduce + %))
-    (partition 2 1)
-    (map #(reduce - %))
-    (filter neg?)
+    (sliding-window-sum 3)
+    changes
+    (filter pos?)
     count
     )
   )
