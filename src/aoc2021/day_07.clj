8bed41b6e8a49b469dbc54d8dcb68312541edcf0
John Flinchbaugh <john@hjsoft.com>
2021-12-07 06:17:41 -0500

    day 7 part 2

--- src/aoc2021/day_07.clj
+++ src/aoc2021/day_07.clj
@@ -16,8 +16,33 @@
      (map #(abs (- % target-pos)))
      (reduce +))))
 
+(defn cost-2 [dist]
+  (->>
+    dist
+    inc
+    range
+    (reduce +)))
+
+(defn part-2 []
+  (let [input (slurp "src/aoc2021/day_07.txt")
+        positions (->>
+                    (->
+                      input
+                      (str/split #","))
+                    (map (comp parse-int str/trim))
+                    sort)
+        target-pos (int (avg positions))]
+    target-pos
+    (->>
+      positions
+      (map #(abs (- % target-pos)))
+      (map cost-2)
+      (reduce +))))
+
 (comment
 
   (part-1);; => 344297
+
+  (part-2);; => 97164301
 
   .)
--- test/aoc2021/day_07_test.clj
+++ test/aoc2021/day_07_test.clj
@@ -4,3 +4,8 @@
 
 (deftest test-part-1
   (is (= 344297 (part-1))))
+
+(deftest test-part-2
+  (is (= 97164301 (part-2))))
+
+
