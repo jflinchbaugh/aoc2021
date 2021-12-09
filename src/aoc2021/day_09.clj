0585833020b1534099922354a8a66b1024638971
John Flinchbaugh <john@hjsoft.com>
2021-12-09 01:54:11 -0500

    day 9 part 1

---
+++ src/aoc2021/day_09.clj
@@ -0,0 +1,38 @@
+(ns aoc2021.day-09
+  (:require [aoc2021.core :refer :all]))
+
+(defn cell [grid row col]
+  (try (nth (nth grid row) col)
+       (catch IndexOutOfBoundsException _ nil)))
+
+(defn surroundings [grid row col]
+  (remove
+   nil?
+   [(cell grid row (dec col))
+    (cell grid row (inc col))
+    (cell grid (dec row) col)
+    (cell grid (inc row) col)]))
+
+(defn part-1 []
+  (let [grid (->>
+              "src/aoc2021/day_09.txt"
+              file->lines
+              (mapv #(mapv (comp parse-int str) %)))
+        length (count grid)
+        width (count (first grid))]
+    (->>
+     (for [row (range 100)
+           col (range 100)]
+       (let [point (cell grid row col)
+             surroundings (surroundings grid row col)]
+         (when (every? #(< point %) surroundings)
+           point)))
+     (remove nil?)
+     (map inc)
+     (reduce +))))
+
+(comment
+
+  (part-1);; => 496
+
+  .)
---
+++ test/aoc2021/day_09_test.clj
@@ -0,0 +1,6 @@
+(ns aoc2021.day-09-test
+  (:require [aoc2021.day-09 :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest test-part-1
+  (is (= 496 (part-1))))
