928b2a4b5d820865be4a9b035fd84572323588ba
John Flinchbaugh <john@hjsoft.com>
2021-12-10 23:46:56 -0500

    tests for day 10

--- test/aoc2021/day_10_test.clj
+++ test/aoc2021/day_10_test.clj
@@ -7,3 +7,20 @@
 
 (deftest test-part-2
   (is (= 3515583998 (part-2))))
+
+(deftest test-check
+  (is (= :good (check "")))
+  (is (= :good (check "()")))
+  (is (= [:incomplete [\)]] (check "(")))
+  (is (= [:corrupt \]] (check "(]"))))
+
+(deftest test-score-auto-complete
+  (is (= 1 (score-auto-complete [\)])))
+  (is (= 2 (score-auto-complete [\]])))
+  (is (= 3 (score-auto-complete [\}])))
+  (is (= 4 (score-auto-complete [\>])))
+  (is (= 6 (score-auto-complete [\) \)])))
+  (is (= 12 (score-auto-complete [\] \]])))
+  (is (= 18 (score-auto-complete [\} \}])))
+  (is (= 24 (score-auto-complete [\> \>])))
+  )
