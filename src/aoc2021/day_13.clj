36f6a954efedf4051cf4ee708e5b662cf43a4f82
John Flinchbaugh <john@hjsoft.com>
2021-12-14 15:02:18 -0500

    day 13 part 2

--- src/aoc2021/day_13.clj
+++ src/aoc2021/day_13.clj
@@ -18,29 +18,57 @@
 
 (defn fold [dots axis crease]
   (->>
-    dots
-    (map
-      (fn [[x y]]
-        (if (#{:x} axis)
-          (if (< x crease)
-            [x y]
-            [(- (* 2 crease) x) y])
-          (if (< y crease)
-            [x y]
-            [x (- (* 2 crease) y)]))))
-    set))
+   dots
+   (map
+    (fn [[x y]]
+      (if (#{:x} axis)
+        (if (< x crease)
+          [x y]
+          [(- (* 2 crease) x) y])
+        (if (< y crease)
+          [x y]
+          [x (- (* 2 crease) y)]))))
+   set))
 
 (defn part-1 []
   (let [{:keys [dots folds]} (->>
-          "src/aoc2021/day_13.txt"
-          file->lines
-          parse)
+                              "src/aoc2021/day_13.txt"
+                              file->lines
+                              parse)
         first-fold (first folds)
         [axis crease] first-fold]
     (count (fold dots axis crease))))
 
+(defn part-2 []
+  (let [{:keys [dots folds]} (->>
+                               "src/aoc2021/day_13.txt"
+                               file->lines
+                               parse)
+        occupied (loop [dots dots
+                        folds folds]
+                   (if (empty? folds)
+                     dots
+                     (let [[axis pos] (first folds)]
+                       (recur (fold dots axis pos) (rest folds)))))
+        max-x (reduce max (map first occupied))
+        max-y (reduce max (map second occupied))]
+    (for [y (range (inc max-y))]
+      (str/join 
+        (for [x (range (inc max-x))]
+          (if (occupied [x y])
+            "#"
+            " "))))))
+
 (comment
 
   (part-1);; => 802
+
+  (part-2)
+;; => ("###  #  # #  # #### ####  ##  #  # ### "
+;;     "#  # # #  #  # #       # #  # #  # #  #"
+;;     "#  # ##   #### ###    #  #    #  # ### "
+;;     "###  # #  #  # #     #   # ## #  # #  #"
+;;     "# #  # #  #  # #    #    #  # #  # #  #"
+;;     "#  # #  # #  # #    ####  ###  ##  ### ")
 
   .)
